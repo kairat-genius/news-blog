a447d0d8062db7d5e74aa5c6f5e5bb6319b1b6e3
kairat <112894056+kairat-genius@users.noreply.github.com>
2024-02-05 07:56:59 +0300

    fastapi

---
+++ backend/fastapi/main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+
+
+app = FastAPI()
+
+
+@app.get('/')
+def home():
+    return {"key": "hello"}
+
+@app.get('/{pk}')
+def get_item(pk: int, q: int = None):
+    return {"key": pk, "q": q}
